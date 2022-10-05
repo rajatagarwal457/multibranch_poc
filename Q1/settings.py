44401a907beb4970af6a8b59d02524190285dc64
Rajat Agarwal <rajat18agarwal@gmail.com>
2022-10-05 14:10:39 +0530

    hosts

--- Q1/settings.py
+++ Q1/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-fyq6a1kvt6yw^l#vax3bc#yw5ni&m946gbvnggesgy1hcqi5dk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
